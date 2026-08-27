c651ae724b27f2c5fd4c68945fdd8b8e01e185d5
reverse <vyxlez@icloud.com>
2026-08-27 10:01:17 +0500

    Load game-first UI animations

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './roulette-fixes.css';
+import './game-ui.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
